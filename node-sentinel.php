a15f56cea6808b15aa0bb867bac8b8a56898ed4c
gloomy13 <maks.czkalski@gmail.com>
2025-05-19 14:32:14 +0200

    New commands

--- node-sentinel.php
+++ node-sentinel.php
@@ -4,11 +4,13 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 use Gloomy13\NodeSentinel\Commands\Node\CheckCommand;
+use Gloomy13\NodeSentinel\Commands\Node\AddCommand;
 
 $application = new Application();
 
 // commands
 $application->add(new CheckCommand());
+$application->add(new AddCommand());
 
 $application->run();
 
